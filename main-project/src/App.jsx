e873c8ad7901b82dd3b5fad3dc68106c3d3e83e3
kunal21raut <kunaldraut2100@gmail.com>
2024-02-15 23:03:59 +0530

    Learned about Tailwind css, components and structure

---
+++ main-project/src/App.jsx
@@ -0,0 +1,20 @@
+import Card from './Components/Card'
+import Counter from './Components/Counter'
+
+function App() {
+
+
+  return (
+    <>
+      <Card />
+      <Counter />
+
+      <h1 className="bg-green-400 p-4 rounded-xl">
+        Hello Tailwind CSS world!
+      </h1>
+
+    </>
+  )
+}
+
+export default App;
